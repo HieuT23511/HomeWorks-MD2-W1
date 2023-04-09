913c8b63f7876b679fe5276528a6a72c0dc35490
HieuT23511 <hieut23511@gmail.com>
2023-04-09 18:35:25 +0700

    Ex3 edited

--- Bai3.ts
+++ Bai3.ts
@@ -6,8 +6,23 @@ function exercise3 (str:string){
     for (let i = arr.length-1; i >=0; i--) {
     arrReverse.push(arr[i]);
     }
-    return arrReverse;
+    return arrReverse.join('');
 }
 
+
+
+// function exercise3(str: string) {
+//     let arr:string[] = str.split('');
+//     let i = 0;
+//     let j = arr.length - 1;
+//     while (i < j) {
+//         let temp: string = arr[i];
+//         arr[i] = arr[j]
+//         arr[j] = temp
+//         i++;
+//         j--;
+//     }
+// return arr.join('');
+// }
 let str:string = 'abcd1234';
 console.log(exercise3(str));
